214c72e6ad48ca43e4941059ce7e1e10c364ff77
Ibrahim <i.alamin5@gmail.com>
2016-06-09 22:45:58 -0400

    API testing

--- doctor/views.py
+++ doctor/views.py
@@ -12,6 +12,24 @@ from django.contrib.auth import authenticate, login, logout
 
 
 
+def create_user(request):
+
+	if request.method == 'POST':
+		form = DoctorForm(request.POST)
+		if form.is_valid():
+			form = form.save()
+			return HttpResponseRedirect('thanks/')
+	else:
+		form = DoctorForm()
+
+	context = { 
+	'form': form
+	}
+
+	return render(request, 'doctor/signup.html', context)
+
+
+
 def signup(request):
 
 	if request.method == 'POST':
@@ -39,24 +57,24 @@ def signin(request):
             login(request, user)
             # Redirect to a success page.
         else:
-            # Return a 'disabled account' error message
-            ...
+            return render(request, 'doctor/signup.html', context)
+
     else:
+    	pass
         # Return an 'invalid login' error message.
 
 
 
 def index(request):
     #return HttpResponse("Hello, world. You're at the doctor index.")
-
     #return render(request, 'doctor/index.html')
+	doctor_list = Doctor.objects.order_by('last_name')
+	context = {
+	'doctor_list': doctor_list,
+		}
 
-    doctor_list = Doctor.objects.order_by('last_name')
-    context = {
-		 'doctor_list': doctor_list,
-	}
+	return render(request, 'doctor/index.html', context)
 
-    return render(request, 'doctor/index.html', context)
 
 
 def doctor(request, doctor_id):
@@ -68,63 +86,60 @@ def doctor(request, doctor_id):
 	return render(request, 'doctor/doctor.html', {'doctor': doctor})
 
 
-# API starts here
+#~~~ API starts here ~~~#
 
-class JSONResponse(HttpResponse):
+# class JSONResponse(HttpResponse): (what is this for????) <----
+#     """
+#     An HttpResponse that renders its content into JSON.
+#     """
+#     def __init__(self, data, **kwargs):
+#         content = JSONRenderer().render(data)
+#         kwargs['content_type'] = 'application/json'
+#         super(JSONResponse, self).__init__(content, **kwargs)
+
+@csrf_exempt   
+def doctor_list(request):
     """
-    An HttpResponse that renders its content into JSON.
+    List all code snippets, or create a new snippet.
     """
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
+    if request.method == 'GET':
+        doctors = Doctor.objects.all()
+        serializer = DoctorSerializer(doctors, many=True)
+        return JSONResponse(serializer.data)
 
-	@csrf_exempt
-	def doctor_list(request):
-	    """
-	    List all code snippets, or create a new snippet.
-	    """
-	    if request.method == 'GET':
-	        doctors = Doctor.objects.all()
-	        serializer = DoctorSerializer(doctors, many=True)
-	        return JSONResponse(serializer.data)
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = DoctorSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data, status=201)
+        return JSONResponse(serializer.errors, status=400)
 
-	    elif request.method == 'POST':
-	        data = JSONParser().parse(request)
-	        serializer = DoctorSerializer(data=data)
-	        if serializer.is_valid():
-	            serializer.save()
-	            return JSONResponse(serializer.data, status=201)
-	        return JSONResponse(serializer.errors, status=400)
+@csrf_exempt
+def doctor_detail(request, pk):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    try:
+        doctor = Doctor.objects.get(pk=pk)
+    except Doctor.DoesNotExist:
+        return HttpResponse(status=404)
 
+    if request.method == 'GET':
+        serializer = DoctorSerializer(doctor)
+        return JSONResponse(serializer.data)
 
-	@csrf_exempt
-	def doctor_detail(request, pk):
-	    """
-	    Retrieve, update or delete a code snippet.
-	    """
-	    try:
-	        doctor = Doctor.objects.get(pk=pk)
-	    except Doctor.DoesNotExist:
-	        return HttpResponse(status=404)
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = DoctorSerializer(doctor, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data)
+        return JSONResponse(serializer.errors, status=400)
 
-	    if request.method == 'GET':
-	        serializer = DoctorSerializer(snippet)
-	        return JSONResponse(serializer.data)
-
-	    elif request.method == 'PUT':
-	        data = JSONParser().parse(request)
-	        serializer = DoctorSerializer(doctor, data=data)
-	        if serializer.is_valid():
-	            serializer.save()
-	            return JSONResponse(serializer.data)
-	        return JSONResponse(serializer.errors, status=400)
-
-	    elif request.method == 'DELETE':
-	        doctor.delete()
-	        return HttpResponse(status=204)
-
-
+    elif request.method == 'DELETE':
+        doctor.delete()
+        return HttpResponse(status=204)
 
 
 
